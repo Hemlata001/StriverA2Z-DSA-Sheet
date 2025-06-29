2812cd4301503c09793dcc47b5627bac8a43baa1
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-06-29 20:25:40 +0530

    SecondLargest.java

---
+++ Arrays - Easy/SecondLargest.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static int secondLargestElement(int[] nums) {
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > maxi) {
+                maxi = nums[i];
+            }
+        }
+        boolean found = false;
+        int secondMaxi = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > secondMaxi && nums[i] < maxi) {
+                secondMaxi = nums[i];
+                found = true;
+                
+            }
+        }
+
+        return found ?secondMaxi : -1;
+    }
+    public static void main(String[] args){
+        int[] nums = {10,10,10,10};
+        System.out.println(secondLargestElement(nums));
+    }
+}  
