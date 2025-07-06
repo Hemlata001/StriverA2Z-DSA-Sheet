2984520c7b9f5a801ef5fa61a7f32201fbf437e8
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-07-06 16:40:20 +0530

    SingleNumber.java

---
+++ Arrays - Easy/SingleNumber.java
@@ -0,0 +1,11 @@
+// 136. Single Number
+class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int val = 0;
+        for(int i = 0;i<nums.length;i++){
+            val = val^nums[i];
+        }
+        return val;
+        
+    }
+}
